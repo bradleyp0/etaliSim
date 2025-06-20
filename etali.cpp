c8e7d60f4be1e8f616996c330e3223c30c3748de
bradley <bradleyphu@gmail.com>
2025-06-20 23:39:42 +0000

    etaliSecond

--- etali.cpp
+++ etali.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include <random>
 
 using namespace std;
 
+int etaliFirst(int, int, int);
+int etaliSecond(int, int, int);
+
 int main(int argc, char **argv)
 {
+	srand(time(0));
 	int lands;
     int nonlands;
     int iterations;
@@ -16,7 +19,7 @@ int main(int argc, char **argv)
 	cout << "enter number of iterations: ";
 	cin >> iterations;
 	cout << "number of successes with etali first: " << etaliFirst(lands, nonlands, iterations) << endl;
-	cout << "number of successes with etali second: " << etaliSecond(lands, nonlands, iterations) << endl;
+	//cout << "number of successes with etali second: " << etaliSecond(lands, nonlands, iterations) << endl;
 	return 0;
 }
 
@@ -25,16 +28,36 @@ int etaliFirst(int land, int nonland, int iteration){
 	int successes=0;
     int deckSize=land+nonland;
 	for (int i=0; i<iteration; i++){
-		srand(time(0));
-		int randomNum = rand() % (deckSize+1);
-        if (randomNum <= nonland) successes++;
+		int randomNum = rand() % (deckSize);
+        if (randomNum < nonland) successes++;
 	}
     return successes;
 }
 
 int etaliSecond(int land, int nonland, int iteration){
     int successes=0;
-
-
+	int deckSize=land+nonland;
+	int currentSize=deckSize;
+	int currentLands=land;
+	int currentNonlands=nonland;
+	bool done=false;
+	for (int i=0; i<iteration; i++){
+		while(!done){
+			int randomNum = rand() % currentSize;
+			if (randomNum < currentNonlands){
+				done=true;
+				currentNonlands--;
+			}
+			else{
+				currentLands--;
+			}
+			currentSize--;
+		}
+		int randomNum = rand() % currentSize;
+        if (randomNum < currentNonlands) successes++;
+		currentSize = deckSize;
+		currentLands = land;
+		currentNonlands = nonland;
+	}
     return successes;
 }
